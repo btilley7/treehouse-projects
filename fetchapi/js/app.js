dbd1525e7d1012a99ad0a120aec9b292cb59b668
Brandt Tilley <brandt.tilley@gmail.com>
2022-06-14 10:19:26 -0400

    Update app.js

--- fetchapi/js/app.js
+++ fetchapi/js/app.js
@@ -6,21 +6,56 @@ const form = document.querySelector('form');
 //  FETCH FUNCTIONS
 // ------------------------------------------
 
-fetch('https://dog.ceo/api/breeds/image/random')
-    .then(response => response.json() )
-    .then(data => console.log(data.message))
+async function fetchData(url) {
+    return fetch(url)
+        .then(res => res.json())
+}
+
+fetchData('https://dog.ceo/api/breeds/list')
+    .then(data => generateOptions(data.message))
+
+fetchData('https://dog.ceo/api/breeds/image/random')
+    .then(data => generateImage(data.message))
+
 
 
 // ------------------------------------------
 //  HELPER FUNCTIONS
 // ------------------------------------------
 
+function generateOptions(data){
+    const options = data.map(item => `
+    <option value='${item}'>${item}</option>
+    `).join('');
+    select.innerHTML = options;
+}
 
+function generateImage(data) {
+    const html = `
+    <img src='${data}' alt=''>
+    <p>Click to view image of ${select.value}s</p>;
+    `;
+    card.innerHTML = html;
+}
 
+function fetchBreedImage(){
+    const breed = select.value;
+    const img = card.querySelector('img');
+    const p = card.querySelector('p');
+
+    fetchData(`https://dog.ceo/api/breeds/${breed}/images/random`)
+        .then(data => {
+            img.src = data.message;
+            img.alt = breed;
+            p.textContent = `Click to view more ${breed}s`;
+        })
+}
 // ------------------------------------------
 //  EVENT LISTENERS
 // ------------------------------------------
 
+select.addEventListener('change', fetchBreedImage);
+card.addEventListener('click', fetchBreedImage);
 
 
 // ------------------------------------------
