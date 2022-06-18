7ff09a5cb5575fbae3efd74ddcd3185952da4abe
Brandt Tilley <brandt.tilley@gmail.com>
2022-06-18 16:15:19 -0400

    Final FetchAPI

--- fetchapi/js/app.js
+++ fetchapi/js/app.js
@@ -8,20 +8,36 @@ const form = document.querySelector('form');
 
 async function fetchData(url) {
     return fetch(url)
+        .then(checkStatus)
         .then(res => res.json())
+        .catch(error => console.log(`Looks like a problem`, error))
 }
 
-fetchData('https://dog.ceo/api/breeds/list')
-    .then(data => generateOptions(data.message))
+Promise.all([
+    fetchData('https://dog.ceo/api/breeds/list'),
+    fetchData('https://dog.ceo/api/breeds/image/random')
 
-fetchData('https://dog.ceo/api/breeds/image/random')
-    .then(data => generateImage(data.message))
+])
+.then(data => {
+    const breedList = data[0].message;
+    const randomImage = data[1].message;
 
+    generateOptions(breedList);
+    generateImage(randomImage)
+})
 
 
 // ------------------------------------------
 //  HELPER FUNCTIONS
 // ------------------------------------------
+
+function checkStatus(response){
+    if(response.ok){
+        return Promise.resolve(response);
+    } else {
+        return Promise.reject(new Error(response.statusText));
+    }
+}
 
 function generateOptions(data){
     const options = data.map(item => `
@@ -33,7 +49,7 @@ function generateOptions(data){
 function generateImage(data) {
     const html = `
     <img src='${data}' alt=''>
-    <p>Click to view image of ${select.value}s</p>;
+    <p>Click to view image of ${select.value}s</p>
     `;
     card.innerHTML = html;
 }
@@ -43,11 +59,11 @@ function fetchBreedImage(){
     const img = card.querySelector('img');
     const p = card.querySelector('p');
 
-    fetchData(`https://dog.ceo/api/breeds/${breed}/images/random`)
+    fetchData(`https://dog.ceo/api/breed/${breed}/images/random`)
         .then(data => {
             img.src = data.message;
             img.alt = breed;
-            p.textContent = `Click to view more ${breed}s`;
+            p.textContent = `Click to view more ${breed}s`
         })
 }
 // ------------------------------------------
@@ -57,8 +73,26 @@ function fetchBreedImage(){
 select.addEventListener('change', fetchBreedImage);
 card.addEventListener('click', fetchBreedImage);
 
+form.addEventListener('submit', postData)
 
 // ------------------------------------------
 //  POST DATA
 // ------------------------------------------
 
+function postData(e) {
+    e.preventDefault();
+
+    const name = document.getElementById('name').value
+    const comment = document.getElementById('comment').value
+    const config = {
+        method: 'POST', 
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({name, comment})
+    }
+    fetch(`https://jsonplaceholder.typicode.com/comments`, config)
+        .then(checkStatus)
+        .then(res => res.json())
+        .then(data => console.log(data))
+}
